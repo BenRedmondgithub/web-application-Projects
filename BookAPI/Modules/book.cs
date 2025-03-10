cf513382348dfd85457ff80377a63076b8506564
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-03-10 11:06:37 +0000

    Initial commit

---
+++ BookAPI/Modules/book.cs
@@ -0,0 +1,12 @@
+namespace MyNamespace
+{
+    public book 
+    {
+        public int id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Author { get; set; }
+        
+    }
+}
