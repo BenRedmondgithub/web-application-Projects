9545845fd34773914d0953f1068c73a4d02dc5e6
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2025-03-13 16:31:43 +0000

    updating

--- BookAPI/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ BookAPI/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7abf43a07aeac4b80a2bf21e4c971941aaba00e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b9f6437c1d1a865e3bcb7cc11fd0afeceaff3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
