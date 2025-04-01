0ace89fe5ccc33dc31be401527cc8ebf7bd69bd8
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2025-04-01 19:17:49 +0100

    adding files

--- BookAPI/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ BookAPI/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8d72846c47fb41963d23d10cd3cbd18b40ce44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f9230205e58f67b346830547c9ebf5ca06e1c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
