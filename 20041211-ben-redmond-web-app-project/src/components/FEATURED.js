da5a9497b9b19a2b6a2b9019bce02a0be6b6153d
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-04-16 13:29:46 +0100

    updated small

--- 20041211-ben-redmond-web-app-project/src/components/FEATURED.js
+++ 20041211-ben-redmond-web-app-project/src/components/FEATURED.js
@@ -25,7 +25,7 @@ function MyCardComponent() {
     <Container sx={{ py: 50, bgcolor: '#D4B6FF' }}>
       <Grid container spacing={4} justifyContent="center">
         {cardData.map((card, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
+          <Grid item xs={6} sm={6} md={4} key={index}>
             <Card sx={{ maxWidth: 345, mx: 'auto' }}>
               <CardMedia
                 component="img"
