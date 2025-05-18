ed09a8a6a44a65b7c8b4e809c51bfc1d6722d9db
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2025-05-18 22:06:04 +0100

    u

---
+++ 20041211-ben-redmond-web-app-project/src/Pages/Signup.jsx
@@ -0,0 +1,5 @@
+import AuthForm from "../components/AuthForm";
+
+const Signup = ({ setIsAuthenticated }) => <AuthForm mode="signup" setIsAuthenticated={setIsAuthenticated} />;
+
+export default Signup;
