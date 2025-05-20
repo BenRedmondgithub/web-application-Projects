69955e7a3053261761fb283b331347c4c50a320d
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-05-20 10:50:56 +0100

    upa

--- 20041211-ben-redmond-web-app-project/server/firebase/firebase.js
+++ 20041211-ben-redmond-web-app-project/server/firebase/firebase.js
@@ -1,7 +1,16 @@
-var admin = require("firebase-admin");
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+const firebaseConfig = {
+  apiKey: "AIzaSyBMzLYg7idv8azHF7ACk6bA6veENemZR_8",
+  authDomain: "ben-redmond-photography.firebaseapp.com",
+  projectId: "ben-redmond-photography",
+  storageBucket: "ben-redmond-photography.firebasestorage.app",
+  messagingSenderId: "887512811757",
+  appId: "1:887512811757:web:d3510c8bfb57cd9c4d24e4",
+  measurementId: "G-S8TVFNK977"
+};
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
